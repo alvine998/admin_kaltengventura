d2d7469ef7c86d9c437c8261e7d802be6135c5f3
alvin <alvinecom2018@gmail.com>
2023-09-14 15:25:04 +0700

    first

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -10,39 +10,32 @@ export default function Login() {
     const router = useRouter()
 
     const handleLogin = async (e: any) => {
+        e.preventDefault();
+        setInfo({ loading: true })
         try {
-            e.preventDefault();
             const formData: any = Object.fromEntries(new FormData(e.target))
-            const result = await login(formData?.email, formData?.password)
-            if (result.result?.user) {
-                Swal.fire({
-                    text: "Login Berhasil",
-                    icon: "success"
-                })
-                localStorage.setItem("uid", result.result?.user?.uid)
-                router.push("/main/dashboard")
-            } else {
-                Swal.fire({
-                    text: "Email atau Password Salah!",
-                    icon: "warning"
-                })
-            }
+            Swal.fire({
+                text: "Email atau Password Salah!",
+                icon: "warning"
+            })
+            setInfo({ loading: false, message: "Berhasil Login" })
         } catch (error) {
             console.log(error);
             Swal.fire({
                 text: "Login Gagal",
                 icon: "error"
             })
+            setInfo({ loading: false, message: "Berhasil Login" })
         }
     }
     return (
         <div className='flex flex-col py-20 px-10'>
-            <h1 className='text-2xl font-semibold text-center'>ADMIN TEMAN K3</h1>
+            <h1 className='text-2xl font-semibold text-center'>ADMIN KALTENGVENTURA</h1>
             <form onSubmit={handleLogin} className='mt-5 sm:px-[500px]'>
                 <Input label='Email' required placeholder='Masukkan Email' type='email' name='email' />
                 <Input label='Password' required placeholder='********' type='password' name='password' />
                 <div className='mt-5'>
-                    <Button>Masuk</Button>
+                    <Button disabled={info.loading}>{info.loading ? "Menunggu..." : "Masuk"}</Button>
                 </div>
             </form>
         </div>
